46a45055c1183a471c6fcfba0f8d66173657e22c
eunji890 <eunjiri@naver.com>
2024-04-24 15:23:01 +0900

    2024 모바일최신기술 중간

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    //swagger
+    implementation group: 'io.springfox', name: 'springfox-swagger2', version: '2.9.2'
+    implementation group: 'io.springfox', name: 'springfox-swagger-ui', version: '2.9.2'
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(18)
+}
